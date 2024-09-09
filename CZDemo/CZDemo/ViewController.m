8dc60ef76cc086ba11f3bdc09490c6f53b0bd91f
xdgcc <xdgcc@gmail.com>
2024-09-09 14:57:58 +0800

    add test code

--- CZDemo/CZDemo/ViewController.m
+++ CZDemo/CZDemo/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface ViewController ()
 
@@ -16,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 50, 540, 258)];
+    [self.view addSubview:imageView];
+    
+    [imageView sd_setImageWithURL:[NSURL URLWithString:@"https://www.baidu.com/img/bd_logo1.png"] placeholderImage:nil];
+    
 }
 
 
